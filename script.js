99979643e0e276291bfb0d7be6a1fcec5c902344
Rita <contact@ritasolo.com>
2018-08-15 17:18:12 -0400

    fourth draft

--- script.js
+++ script.js
@@ -7,21 +7,16 @@ $(function () {
     console.log('ready!')
     // let planetClasses;
 
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
-    });
-
     $('input:radio').click(function () {
         let planetClasses = $(this).attr('class');
         planets = planetClasses.split(' ');
         console.log(planets);
         planetArray = planetArray.concat(planets);
+    });
+
+    $('.quiz-button').on("click", function() {
+        $('.take-quiz').hide();
+        $('#q1').removeClass('hide');
     });
 
     $('#q1 input:radio').click(function() {
@@ -34,75 +29,111 @@ $(function () {
     });
 
     $('#q2 input:radio').click(function () {
-        $('#q2').addClass('hide');
-        $('#q3').removeClass('hide');
+        setTimeout(
+            function () {
+                $('#q2').addClass('hide');
+                $('#q3').removeClass('hide');
+            },
+            500);
     });
 
     $('#q3 input:radio').click(function () {
-        $('#q3').addClass('hide');
-        $('#q4').removeClass('hide');
+        setTimeout(
+            function () {
+                $('#q3').addClass('hide');
+                $('#q4').removeClass('hide');
+            },
+            500);
     });
 
     $('#q4 input:radio').click(function () {
-        $('#q4').addClass('hide');
-        $('#q5').removeClass('hide');
+        setTimeout(
+            function () {
+                $('#q4').addClass('hide');
+                $('#q5').removeClass('hide');
+            },
+            500);
     });
 
     $('#q5 input:radio').click(function () {
-        $('#q5').addClass('hide');
-        $('#q6').removeClass('hide');
+        setTimeout(
+            function () {
+                $('#q5').addClass('hide');
+                $('#q6').removeClass('hide');
+            },
+            500);
     });
 
     $('#q6 input:radio').click(function () {
-        $('#q6').addClass('hide');
-        $('#q7').removeClass('hide');
+        setTimeout(
+            function () {
+                $('#q6').addClass('hide');
+                $('#q7').removeClass('hide');
+            },
+            500);
     });
 
     $('#q7 input:radio').click(function () {
-        $('#q7').addClass('hide');
-        $('#q8').removeClass('hide');
+        setTimeout(
+            function () {
+                $('#q7').addClass('hide');
+                $('#q8').removeClass('hide');
+            },
+            500);
     });
 
     $('#q8 input:radio').click(function () {
-        $('#q8').addClass('hide');
-        $('#q9').removeClass('hide');
+        setTimeout(
+            function () {
+                $('#q8').addClass('hide');
+                $('#q9').removeClass('hide');
+            },
+            500);
     });
 
     $('#q9 input:radio').click(function () {
-        $('#q9').addClass('hide');
-        $('#q10').removeClass('hide');
+        setTimeout(
+            function () {
+                $('#q9').addClass('hide');
+                $('#q10').removeClass('hide');
+            },
+            500);
+    });
+
+    $('#q10 input:radio').click(function () {
+        setTimeout(
+            function () {
+                $('#q10').addClass('hide');
+                $('input:submit').removeClass('hide');
+            },
+            500);
     });
 
     $('form').on('submit', function (e) {
         e.preventDefault();
-        
-        function count() {
-            let mf = 1;
-            let m = 0;
-            let item;
-            for (let i = 0; i < planetArray.length; i++) {
-                for (let j = i; j < planetArray.length; j++) {
-                    if (planetArray[i] == planetArray[j])
-                        m++;
-                    if (mf < m) {
-                        mf = m;
-                        item = planetArray[i];
-                    }
-                }
-                m = 0;
-            }
-            return item;
-        };
 
-        let userAnswer = count();
+        function count(arr) {
+            return arr.sort((a, b) =>
+                arr.filter(value => value === a).length 
+                - arr.filter(value => value === b).length
+            ).pop();
+        }
+
+        function mode(arr) {
+            return arr.sort(function (a, b) {
+                return arr.filter(function (v) { return v === a }).length
+                    - arr.filter(function (v) { return v === b }).length
+            }).pop();
+        }
+
+
+        let userAnswer = count(planetArray);
         console.log(`you got ${userAnswer}`);
         $(this).addClass('hide');
-        $(`.${userAnswer}`).removeClass('hide'); 
-    })
-
-    $('.whiteOut').on("click", function(e){
-        e.preventDefault();
-        $('form').attr("top", "-5vw");
+        $(`.${userAnswer}`).removeClass('hide').addClass('flex');
+        $('.solarsystem').hide(); 
+        $('.title').hide();
+        $('.you-are').removeClass('hide');
     })
 
 });
